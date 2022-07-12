5885cd181f5097541415a225194f5ff83a0dfeff
manipapan2 <manipapan2@gmail.com>
2022-07-12 19:10:48 +0430

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,61 @@
-function check(){
-    var pass = document.getElementById("wer").value;
-    var cpass = document.getElementById("wet").value;
+function checkname(){
+var pass = document.getElementById("wer").value;
+var cpass = document.getElementById("wet").value;
+var nameId = document.getElementById("nameId").value;
+var emailId = document.getElementById("emailId").value; 
 
 
+    if(nameId === ""){
 
+        document.getElementById("inA").style.display = "flex"
+
+    }
+    else{
+        document.getElementById("inA").style.display = "none"
+    }
+}    
+
+function checkpass(){
+var pass = document.getElementById("wer").value;
+var cpass = document.getElementById("wet").value;
+var nameId = document.getElementById("nameId").value;
+var emailId = document.getElementById("emailId").value; 
+
+    if(pass === ""){
+        document.getElementById("inB").style.display = "flex"
+
+    }
+    else{
+        document.getElementById("inB").style.display = "none"
+    }
+}
+
+
+function checkcpass(){
+var pass = document.getElementById("wer").value;
+var cpass = document.getElementById("wet").value;
+var nameId = document.getElementById("nameId").value;
+var emailId = document.getElementById("emailId").value; 
+    
+    if(pass === cpass){
+        document.getElementById("inC").style.display = "none"
+        }
+    else{
+        document.getElementById("inC").style.display = "flex"
+    }
+}
+
+function checkemail(){
+var pass = document.getElementById("wer").value;
+var cpass = document.getElementById("wet").value;
+var nameId = document.getElementById("nameId").value;
+var email = document.getElementById("emailId").value; 
+        
+    if(email === ""){
+        document.getElementById("inD").style.display = "flex"
+        
+    }
+    else{
+        document.getElementById("inD").style.display = "none"
+    }
 }
