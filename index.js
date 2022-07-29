b8f8220745d7904196731c94083fc1b5f90d64a9
manipapan2 <105167359+manipapan2@users.noreply.github.com>
2022-07-29 20:03:16 +0430

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+//onkeyup
+document.getElementById("nameId").addEventListener("keyup" , checkname);
+document.getElementById("wer").addEventListener("keyup" , checkpass);
+document.getElementById("wer").addEventListener("keyup" , checkcpass);
+document.getElementById("wet").addEventListener("keyup" , checkcpass);
+document.getElementById("emailId").addEventListener("keyup" , checkemail);
+document.getElementById("submitButton").addEventListener("click" , checkname);
+document.getElementById("submitButton").addEventListener("click" , checkcpass);
+document.getElementById("submitButton").addEventListener("click" , checkpass);
+document.getElementById("submitButton").addEventListener("click" , checkemail);
+//============================================================================================
+
+//functions
 function checkname(){
-    var nameId = document.getElementById("nameId").value;; 
+    var nameId = document.getElementById("nameId").value;
     
     
         if(nameId === ""){
@@ -7,6 +20,7 @@ function checkname(){
             document.getElementById("inA").style.color = "Red"
             document.getElementById("inA").style.fontWeight = "bold"
             document.getElementById("inA").style.display = "flex"
+            return false;
     
         }
         
@@ -16,60 +30,60 @@ function checkname(){
             document.getElementById("inA").style.fontWeight = "bold"
             document.getElementById("inA").style.display = "flex"
         }
-    }    
-    
-    function checkpass(){
-    var pass = document.getElementById("wer").value;
+}    
 
+
+function checkpass(){
+    var pass = document.getElementById("wer").value;
     
+        
         if(pass === ""){
             document.getElementById("inB").innerHTML = "Please Enter Your Password"
             document.getElementById("inB").style.color = "Red"
             document.getElementById("inB").style.fontWeight = "bold"
             document.getElementById("inB").style.display = "flex"
-    
+        
         }
         else{
             document.getElementById("inB").innerHTML = "Good"
             document.getElementById("inB").style.color = "rgb(6, 250, 6)"
             document.getElementById("inB").style.fontWeight = "bold"
             document.getElementById("inB").style.display = "flex"
-    
+        
         }
-    }
-    
-    
-    function checkcpass(){
+}
+
+
+function checkcpass(){
     var pass = document.getElementById("wer").value;
     var cpass = document.getElementById("wet").value;
-        
-    
-            if(pass === '' && cpass === ''){
-                document.getElementById("inC").style.display = "flex"
-    
-            }
-    
-            else if(pass === cpass){
-                document.getElementById("inC").innerHTML = "Good"
-                document.getElementById("inC").style.color = "rgb(6, 250, 6)"
-                document.getElementById("inC").style.fontWeight = "bold"
-                document.getElementById("inC").style.display = "flex"
-        
-                }
-    
+                
+        if(pass === '' && cpass === ''){
+            document.getElementById("inC").style.display = "flex"
+
+        }
+            
+        else if(pass === cpass){
+            document.getElementById("inC").innerHTML = "Good"
+            document.getElementById("inC").style.color = "rgb(6, 250, 6)"
+            document.getElementById("inC").style.fontWeight = "bold"
+            document.getElementById("inC").style.display = "flex"
+                
+        }
+            
         else{
             document.getElementById("inC").innerHTML = "Please Confirm Your password Corectly"
             document.getElementById("inC").style.color = "Red"
             document.getElementById("inC").style.fontWeight = "bold"
             document.getElementById("inC").style.display = "flex"
-    
+            
         }
-    }
-    
-    function checkemail(){
+}
+
+
+function checkemail(){
     var email = document.getElementById("emailId").value; 
         
-    
         if(email === ""){
             document.getElementById("inD").innerHTML = "Please Enter Your Gmail"
             document.getElementById("inD").style.color = "Red"
@@ -84,8 +98,5 @@ function checkname(){
             document.getElementById("inD").style.display = "flex"
         }
     }
-
-
-    
-    
-    
+ 
+//============================================================================================
