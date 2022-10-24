7aede1198d13a7de479022052070e81b125c900a
manipapan2 <105167359+manipapan2@users.noreply.github.com>
2022-10-24 22:57:13 +0330

    Update index.js

--- index.js
+++ index.js
@@ -203,47 +203,86 @@ function checkemail(){
 
 function checknamel(){
     var nameId = document.getElementById("nameId").value;
-        
-        
+    var nameIdlen = document.getElementById("nameId").length;
+    var num = [1,2,3,4,5,6,7,8,9,0];
+    
+    
     if(nameId === ""){
         document.getElementById("inA").innerHTML = "Please Enter Your Username";
         document.getElementById('inA').classList.remove("acces");
         document.getElementById('inA').classList.add("inA");
         document.getElementById("nameId").classList.add("larzesh");
-        setTimeout(pak,200);
+        setTimeout(pak,200)
         return false;
-        
-    }
-            
-    else{
-        document.getElementById("inA").innerHTML = "Good";
-        document.getElementById('inA').classList.add("acces");
-        document.getElementById('inA').classList.remove("inA");
-        return true;
     
     }
-}    
+
+    if(nameId.length>12){
+        document.getElementById('inA').classList.remove("acces");
+        document.getElementById('inA').classList.add("inA");
+        document.getElementById("inA").innerHTML = "Your Name Can Not Be More Than 12 Character";
+        document.getElementById("nameId").classList.add("larzesh");
+        setTimeout(pak,200)
+        return false;
+    }
+
+    if(nameId.length<3){
+        document.getElementById('inA').classList.remove("acces");
+        document.getElementById('inA').classList.add("inA");
+        document.getElementById("inA").innerHTML = "Your Username Should Be At Least 3 Character";
+        document.getElementById("nameId").classList.add("larzesh");
+        setTimeout(pak,200)
+        return false;
+    }
+    
+    else{
+        document.getElementById('inA').classList.add("acces");
+        document.getElementById('inA').classList.remove("inA");
+        document.getElementById("inA").innerHTML = "Good";
+        return true;
+
+    }
+}     
     
     
 function checkpassl(){
     var passasl = document.getElementById("passasl").value;
+    
         
-            
     if(passasl === ""){
-        document.getElementById("inB").innerHTML = "Please Enter Your Password"
+        document.getElementById("inB").innerHTML = "Please Enter Your Password";
         document.getElementById('inB').classList.remove("acces");
         document.getElementById('inB').classList.add("inB");
         document.getElementById("passasl").classList.add("larzesh");
         setTimeout(pak,200)
         return false;
-            
+        
     }
+    if(passasl.length<8){
+        document.getElementById("inB").innerHTML = "Your Password Must Be At Least 8 Character";
+        document.getElementById('inB').classList.remove("acces");
+        document.getElementById('inB').classList.add("inB");
+        document.getElementById("passasl").classList.add("larzesh");
+        setTimeout(pak,200)
+        return false;
+
+    }
+
+    if(!passasl.match("([A-Za-z]+[0-9]|[0-9]+[A-Za-z])[A-Za-z0-9]*")){
+        document.getElementById("inB").innerHTML = "Your Password Must Contains At Least 1 Character And 1 Letter";
+        document.getElementById('inB').classList.remove("acces");
+        document.getElementById('inB').classList.add("inB");
+        document.getElementById("passasl").classList.add("larzesh");
+        setTimeout(pak,200)
+        return false;
+    }
+
+
     else{
-        document.getElementById("inB").innerHTML = "Good"
+        document.getElementById("inB").innerHTML = "Good";
         document.getElementById('inB').classList.add("acces");
         document.getElementById('inB').classList.remove("inB");
         return true;
-            
     }
 }
     
@@ -251,38 +290,39 @@ function checkpassl(){
 function checkcpassl(){
     var passasl = document.getElementById("passasl").value;
     var passc = document.getElementById("passc").value;
-                    
-    if(passasl === '' && passc === ''){
-        document.getElementById("inC").style.display = "flex";
-        document.getElementById("passasl").classList.add("larzesh");
-        setTimeout(pak,200);
-        return false;
-    
-    }
                 
+    if(passasl === '' && passc === ''){
+        document.getElementById("inC").style.display = "flex"
+        document.getElementById("passc").classList.add("larzesh");
+        setTimeout(pak,200)
+        return false;
+
+    }
+            
     else if(passasl === passc){
         document.getElementById("inC").innerHTML = "Good";
-        document.getElementById('inC').classList.add("acces");
         document.getElementById('inC').classList.remove("inC");
+        document.getElementById('inC').classList.add("acces");
         return true;
-                    
-    }
                 
+    }
+    
     else{
-        document.getElementById("inC").innerHTML = "Please Confirm Your password Corectly";
+        document.getElementById("inC").innerHTML = "Please Confirm Your password Correctly";
         document.getElementById('inC').classList.remove("acces");
         document.getElementById('inC').classList.add("inC");
         document.getElementById("passc").classList.add("larzesh");
         setTimeout(pak,200)
         return false;
-                
-        }
+            
+    }
 }
     
     
 function checkemaill(){
-    var email = document.getElementById("emailId").value; 
-            
+    var email = document.getElementById("emailId").value;
+    
+        
     if(email === ""){
         document.getElementById("inD").innerHTML = "Please Enter Your Gmail";
         document.getElementById('inD').classList.remove("acces");
@@ -290,14 +330,87 @@ function checkemaill(){
         document.getElementById("emailId").classList.add("larzesh");
         setTimeout(pak,200)
         return false;
-                
     }
+
+
+    if(email.includes(" ")){
+        document.getElementById("inD").innerHTML = "Please Enter Your Gmail Correctly";
+        document.getElementById('inD').classList.remove("acces");
+        document.getElementById('inD').classList.add("inD");
+        document.getElementById("emailId").classList.add("larzesh");
+        setTimeout(pak,200)
+        return false;
+    }
+
+
+    if(!email.includes("@")){
+        document.getElementById("inD").innerHTML = "Please Enter Your Gmail Correctly";
+        document.getElementById('inD').classList.remove("acces");
+        document.getElementById('inD').classList.add("inD");
+        document.getElementById("emailId").classList.add("larzesh");
+        setTimeout(pak,200)
+        return false;
+    }
+        
+    if(email.includes("@")){
+        let rool = email.split("@") 
+
+        if(rool[1].match("(?=.*[A-Z])")){
+            document.getElementById("inD").innerHTML = "Only Lowercase Is Allowed";
+            document.getElementById('inD').classList.remove("acces");
+            document.getElementById('inD').classList.add("inD");
+            document.getElementById("emailId").classList.add("larzesh");
+            setTimeout(pak,200)
+            return false;
+        }
+
+        if(rool[1] != "gmail.com"){
+            document.getElementById("inD").innerHTML = "Please Enter Your Gmail Correctly";
+            document.getElementById('inD').classList.remove("acces");
+            document.getElementById('inD').classList.add("inD");
+            document.getElementById("emailId").classList.add("larzesh");
+            setTimeout(pak,200)
+            return false;
+        }
+
+        if(rool[0].includes("@")){
+            document.getElementById("inD").innerHTML = "Please Enter Your Gmail Correctly";
+            document.getElementById('inD').classList.remove("acces");
+            document.getElementById('inD').classList.add("inD");
+            document.getElementById("emailId").classList.add("larzesh");
+            setTimeout(pak,200)
+            return false;
+        }
+
+
+        if(rool[0] == ""){
+            document.getElementById("inD").innerHTML = "Please Enter Your Gmail Correctly";
+            document.getElementById('inD').classList.remove("acces");
+            document.getElementById('inD').classList.add("inD");
+            document.getElementById("emailId").classList.add("larzesh");
+            setTimeout(pak,200)
+            return false;
+        }
+
+
+
+        if(rool[1] == "gmail.com"){
+            document.getElementById("inD").innerHTML = "Good";
+            document.getElementById('inD').classList.add("acces");
+            document.getElementById('inD').classList.remove("inD");
+            return true;
+        }
+
+
+            }
+            
+
     else{
-        document.getElementById("inD").innerHTML = "Good"
+        document.getElementById("inD").innerHTML = "Good";
         document.getElementById('inD').classList.add("acces");
         document.getElementById('inD').classList.remove("inD");
         return true;
-    
+
     }
 }
 
